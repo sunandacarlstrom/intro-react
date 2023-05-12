bc6c415906b17c7447a4bb9e233c1edfa0b1b9d1
Sunanda Carlström <carlstrom-3@hotmail.com>
2023-05-12 13:51:13 +0200

    mall för React Starter Project klar

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// retunera endast den HTML som vi vill skapa med hjälp av React (omvandlar detta till jsx)
+const App = () => {
+    return (
+        <section>
+        <h1>Välkommen till min mall för att påbörja ett React projekt!</h1>
+        <h2>Detta är fantastiskt!</h2>
+    </section>
+    )
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+// Talar om att root är applikationsroten för min React appliaktion
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+root.render(<App />);
